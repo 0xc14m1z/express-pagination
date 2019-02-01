208276a9ade973b690581800ed683cd39c03a830
Luca Ramundo <0xc14m1z@gmail.com>
2019-02-01 23:57:44 +0100

    Add implementation for isPositiveInteger helper method

--- src/isPositiveInteger.js
+++ src/isPositiveInteger.js
@@ -1,3 +1,7 @@
-const isPositiveInteger = value => {}
+const isPositiveInteger = value =>
+  (typeof value === 'number' && !Number.isNaN(value) || value instanceof Number)
+    && Number.isFinite(value)
+    && Number.isInteger(value)
+    && value > 0
 
 export default isPositiveInteger
