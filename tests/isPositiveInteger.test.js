d80dfa21e35e6f650e80dec60e6227719169c5f2
Luca Ramundo <0xc14m1z@gmail.com>
2019-02-01 23:54:22 +0100

    Draft tests implementation

--- tests/isPositiveInteger.test.js
+++ tests/isPositiveInteger.test.js
@@ -4,18 +4,54 @@ import isPositiveInteger from 'src/isPositiveInteger'
 
 export default () => {
 
-  it('should return true if positive integer is given')
+  it('should return true if positive integer is given', () => {
+    expect(isPositiveInteger(42)).to.be.true
+  })
 
-  it('should return false if negative integer is given')
-  it('should return false if zero is given')
-  it('should return false if float is given')
-  it('should return false if NaN is given')
+  it('should return false if negative integer is given', () => {
+    expect(isPositiveInteger(-42)).to.be.false
+  })
 
-  it('should return false if an array is given')
-  it('should return false if an object is given')
-  it('should return false if a function is given')
-  it('should return false if a boolean is given')
-  it('should return false if null is given')
-  it('should return false if nothing is given')
+  it('should return false if zero is given', () => {
+    expect(isPositiveInteger(0)).to.be.false
+  })
+
+  it('should return false if float is given', () => {
+    expect(isPositiveInteger(4.2)).to.be.false
+  })
+
+  it('should return false if NaN is given', () => {
+    expect(isPositiveInteger(NaN)).to.be.false
+  })
+
+  it('should return false if an array is given', () => {
+    expect(isPositiveInteger([1, 2, 3])).to.be.false
+  })
+
+  it('should return false if an object is given', () => {
+    expect(isPositiveInteger({ a: 1, b: 2 })).to.be.false
+  })
+
+  it('should return false if a function is given', () => {
+    expect(isPositiveInteger(() => 42)).to.be.false
+  })
+
+  it('should return false if a boolean is given', () => {
+    expect(isPositiveInteger(true)).to.be.false
+    expect(isPositiveInteger(false)).to.be.false
+  })
+
+  it('should return false if null is given', () => {
+    expect(isPositiveInteger(null)).to.be.false
+  })
+
+  it('should return false if nothing is given', () => {
+    expect(isPositiveInteger()).to.be.false
+    expect(isPositiveInteger(undefined)).to.be.false
+  })
+
+  it('should return false if a string is given', () => {
+    expect(isPositiveInteger('42')).to.be.false
+  })
 
 }
