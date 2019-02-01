f279369eeec8eeab57d49915b1602cd235767f23
Luca Ramundo <0xc14m1z@gmail.com>
2019-02-01 23:41:02 +0100

    Draft tests implementation

--- tests/pagination.test.js
+++ tests/pagination.test.js
@@ -1,30 +1,184 @@
-import { express } from 'chai'
+import { expect } from 'chai'
 
 import pagination from 'src/pagination'
 
 export default () => {
 
-  it('should be a function')
-  it('should return the middleware generator function')
-  it('should have default configuration')
-  it('should apply configuration if given')
+  it('should be a function', () => {
+    expect(pagination).to.be.a('function')
+  })
+
+  it('should return the middleware generator function', () => {
+    expect(pagination()).to.be.a('function')
+  })
+
+  it('should apply custom configuration if given', () => {
+    const config = {
+            input: {
+              page: 'pagina',
+              perPage: 'per-pagina'
+            },
+            output: {
+              property: 'paginazione',
+              page: 'page',
+              perPage: 'limit',
+              from: 'offset',
+              defaultPerPage: 50
+            }
+          },
+          generator = pagination(config),
+          middleware = generator(),
+          request = { params: { pagina: 3, 'per-pagina': 21 } },
+          response = {},
+          next = () => {}
+
+    middleware(request, response, next)
+    expect(request).to.have.property('paginazione')
+    expect(request.paginazione).to.have.property('page', 3)
+    expect(request.paginazione).to.have.property('limit', 21)
+    expect(request.paginazione).to.have.property('offset', 42)
+  })
 
   describe('middleware generator', () => {
-    it('should be a function')
-    it('should throw if results per page is not an positive integer')
-    it('should return a middleware function')
+    let addPagination
+
+    before(() => {
+      addPagination = pagination()
+    })
+
+    it('should be a function', () => {
+      expect(addPagination).to.be.a('function')
+    })
+
+    it('should throw if results per page is not an positive integer', () => {
+      expect(() => addPagination('42')).to.throw
+      expect(() => addPagination({})).to.throw
+      expect(() => addPagination([])).to.throw
+      expect(() => addPagination(4.2)).to.throw
+      expect(() => addPagination(() => 42)).to.throw
+      expect(() => addPagination(NaN)).to.throw
+      expect(() => addPagination(-5)).to.throw
+      expect(() => addPagination(0)).to.throw
+      expect(() => addPagination(true)).to.throw
+    })
+
+    it('should return a middleware function', () => {
+      expect(addPagination()).to.be.a('function')
+    })
 
     describe('middleware', () => {
-      it('should take three parameters')
-      it('should call next when finishes')
-      it('should set a property on the request named as specified in configuration')
-      it('should set an offset property named as specified in configuration')
-      it('should set 1 as default page number if no page param is given')
-      it('should use given page number via GET request')
-      it('should use given page number via POST request')
-      it('should set PER_PAGE as default results per page if no results per page param is given')
-      it('should use given results per page number via GET request')
-      it('should use given results per page number via POST request')
+      let middleware
+      const RESULTS_PER_PAGE = 42
+
+      before(() => {
+        middleware = addPagination(RESULTS_PER_PAGE)
+      })
+
+      it('should take three parameters', () => {
+        expect(middleware.length).to.equal(3)
+      })
+
+      it('should call next when finishes', () => {
+        let called = false
+        const request = {},
+              response = {},
+              next = () => called = true
+
+        middleware(request, response, next)
+        expect(called).to.be.true
+      })
+
+      it('should set a property on the request named as specified in configuration', () => {
+        const request = {},
+              response = {},
+              next = () => {}
+
+        middleware(request, response, next)
+        expect(request).to.have.property('pagination')
+        expect(request.pagination).to.have.all.keys(['page', 'perPage', 'from'])
+      })
+
+      it('should set a from property named as specified in configuration', () => {
+        const request = { params: { page: 3, perPage: 21 } },
+              response = {},
+              next = () => {}
+
+        middleware(request, response, next)
+        expect(request.pagination).to.have.property('from', 42)
+
+      })
+
+      it('should set 1 as default page number if no page param is given', () => {
+        const request = {},
+              response = {},
+              next = () => {}
+
+        middleware(request, response, next)
+        expect(request.pagination).to.have.property('page', 1)
+      })
+
+      it('should use given page number from query string parameters', () => {
+        const request = { query: { page: 10 } },
+              response = {},
+              next = () => {}
+
+        middleware(request, response, next)
+        expect(request.pagination).to.have.property('page', 10)
+      })
+
+      it('should use given page number from url parameters', () => {
+        const request = { params: { page: 20 } },
+              response = {},
+              next = () => {}
+
+        middleware(request, response, next)
+        expect(request.pagination).to.have.property('page', 20)
+      })
+
+      it('should use given page number from request body', () => {
+        const request = { body: { page: 30 } },
+              response = {},
+              next = () => {}
+
+        middleware(request, response, next)
+        expect(request.pagination).to.have.property('page', 30)
+      })
+
+      it('should set RESULTS_PER_PAGE as default results per page if no results per page param is given', () => {
+        const request = {},
+              response = {},
+              next = () => {}
+
+        middleware(request, response, next)
+        expect(request.pagination).to.have.property('perPage', CUSTOM_PER_PAGE)
+      })
+
+      it('should use given results per page number from query string parameters', () => {
+        const request = { query: { perPage: 10 } },
+              response = {},
+              next = () => {}
+
+        middleware(request, response, next)
+        expect(request.pagination).to.have.property('perPage', 10)
+      })
+
+      it('should use given results per page number from url parameters', () => {
+        const request = { params: { perPage: 20 } },
+              response = {},
+              next = () => {}
+
+        middleware(request, response, next)
+        expect(request.pagination).to.have.property('perPage', 20)
+      })
+
+      it('should use given results per page number from request body', () => {
+        const request = { body: { perPage: 30 } },
+              response = {},
+              next = () => {}
+
+        middleware(request, response, next)
+        expect(request.pagination).to.have.property('perPage', 30)
+      })
 
     })
   })
